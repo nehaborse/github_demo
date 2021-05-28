17993e8d007e022caf8660b783c46777b69f969c
nehaborse29 <nehaborse99@gmail.com>
2021-05-28 14:46:26 +0530

    Added fuc headers

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+# Addition implementation
 def add(x,y):
     pass
-	
+
+# Subtract implementation
 def subtract(x,y):
     pass
 	
+# Multiply implementation	
 def multiply(x,y):
     pass
 	
+# Divide implementation	
 def divide(x,y):
     pass
